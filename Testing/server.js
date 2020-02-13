4e59714f56cc69f0399d3e3ead04e9d432abc3d5
WiktorLidwin <31045748+WiktorLidwin@users.noreply.github.com>
2020-02-12 23:08:22 -0500

    Revert "CHANGES YT API KEY"
    This reverts commit b91b4edec71b0256e6929381ee31ecba9f2fe402.

--- Testing/server.js
+++ Testing/server.js
@@ -202,7 +202,7 @@ io.sockets.on('connection', function (socket) {
             var index = 0;
             var found = false;
             var youTube = new YouTube();
-            youTube.setKey('AIzaSyDMpuHlzDjMEAFPl4D9uim-Tat0l65IfW8');
+            youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
             ytsearch();
             function ytsearch() {
                 console.log("called yt search");
