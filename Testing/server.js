b91b4edec71b0256e6929381ee31ecba9f2fe402
WiktorLidwin <31045748+WiktorLidwin@users.noreply.github.com>
2020-02-12 22:54:18 -0500

    CHANGES YT API KEY

--- Testing/server.js
+++ Testing/server.js
@@ -202,7 +202,7 @@ io.sockets.on('connection', function (socket) {
             var index = 0;
             var found = false;
             var youTube = new YouTube();
-            youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
+            youTube.setKey('AIzaSyDMpuHlzDjMEAFPl4D9uim-Tat0l65IfW8');
             ytsearch();
             function ytsearch() {
                 console.log("called yt search");
